a06b90e4a51e3c0734fb1ba5fe51456b6b5a8a14
children2018 <Evj86@163.com>
2019-07-07 13:08:36 +0800

    第一次提交代码

---
+++ src/main/java/com/spring/springboot/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.spring.springboot.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+ 
+@Controller
+@RequestMapping("/app")
+public class TestController {
+ 
+	@RequestMapping("/test")
+	@ResponseBody
+	public String testDemo() {
+		
+		return "Hello World!";
+	}
+ 
+}
